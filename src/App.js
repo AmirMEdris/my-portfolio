33286c249a8e963b796db62943bd95d49ce3af1a
Amir Edris <amiredris25@gmail.com>
2023-09-22 13:28:40 -0400

    added updated text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Welcome to My Portfolio</h1>
         <p>
-          Hello! I'm Amir Malik Edris. This is a brief introduction about me.
+          Hello I'm Amir Malik Edris. This is a brief introduction about me.
           I'm a passionate developer with experience in various technologies.
           Stay tuned for more updates on my projects and blogs!
         </p>
